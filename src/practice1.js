ae79cbf8d8417bbe3ae93ab7debd89a564494f38
chentaiyu0336 <2868510447@qq.com>
2020-09-11 13:06:48 +0800

    [cty] finish ES6 homework

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,13 @@
-const parseData
+const parseData = (input) => {
+  let { data, column } = input;
+  let result = [];
+  data.forEach(item => {
+    let temp = {};
+    column.forEach((val, index) => {
+      temp[val.name] = item[index];
+    })
+    result.push(temp);
+  });
+  return result;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,8 @@
-const inject
+const inject = (items, sections) => {
+  let result = items;
+  sections.sort((a, b) => a.index - b.index).forEach((it, i) => {
+    result.splice(it.index + i, 0, it.content);
+  });
+  return result;
+}
 export { inject };
