3901fa95e27e29e2753a7f7db20c19e29ca7f1ab
kevindongzg <dongkevin94@gmail.com>
2020-09-21 20:53:18 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,4 +1,6 @@
+// TODO 试着尝试一下reduce
 const parseData = (input) => {
+  // TODO 解构可以优化到函数接受参数中
   let { data, column } = input;
   let result = [];
   data.forEach(item => {
--- src/practice2.js
+++ src/practice2.js
@@ -1,4 +1,5 @@
 const inject = (items, sections) => {
+  // TODO 没有必要重新声明，item有语义性
   let result = items;
   sections.sort((a, b) => a.index - b.index).forEach((it, i) => {
     result.splice(it.index + i, 0, it.content);
